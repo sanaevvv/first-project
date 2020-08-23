19be217e9544c52b590daba00a1b907bec53e6b6
sanaevvv <sanaevvv@gmail.com>
2020-08-23 21:14:20 +0900

    productの方にもcategoriesと関連付けをする

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
      has_many   :users_products, dependent: :destroy
      has_many   :users, through: :users_products
+
+     has_many   :product_categories, dependent: :destroy
+     has_many   :categories, through: :product_categories
 end
